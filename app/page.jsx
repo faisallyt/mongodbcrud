2af99bb79691aa85e017da51ff7cd2bca39ce755
faisallyt <96649676+faisallyt@users.noreply.github.com>
2024-06-22 02:19:33 +0530

    Ui Done

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import TopicList from "@/components/TopicsList";
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <TopicList />
+    </>
+  );
 }
